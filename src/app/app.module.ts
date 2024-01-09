62fc6411759e26bcbd5dc0cf6135a4e35faae929
codeFabFit <fabgar14@gmail.com>
2024-01-08 21:08:27 -0500

    added labels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModel } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
+// import { FormsModule, NgModel } from "@angular/forms";
+// import { BrowserModule } from "@angular/platform-browser";
+// import { AppComponent } from "./app.component";
+// import { DashboardComponent } from "./component/dashboard/dashboard.component";
+// import {HttpClientModule} from '@angular/common/http'
 
-@new NgModel({
-    declare: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    bootstrap: [AppComponent]
-})
+// @new NgModel({
+//     declarations: [
+//         AppComponent,
+//         DashboardComponent
+//     ],
+//     imports: [
+//         BrowserModule,
+//         FormsModule,
+//         HttpClientModule
+//     ],
+//     providers: [],
+//     bootstrap: [AppComponent]
+// })
 
-export class AppModule {}
+// export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 
 enableProdMode();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err))
+  // platformBrowserDynamic().bootstrapModule(AppModule)
+  //   .catch(err => console.error(err))
