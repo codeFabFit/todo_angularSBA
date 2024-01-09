638259b398eea990f8627da619022306e94cc6a3
codeFabFit <fabgar14@gmail.com>
2024-01-09 13:33:15 -0500

    removed error within code not sure the issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-// import { FormsModule, NgModel } from "@angular/forms";
-// import { BrowserModule } from "@angular/platform-browser";
-// import { AppComponent } from "./app.component";
-// import { DashboardComponent } from "./component/dashboard/dashboard.component";
+import { FormsModule, NgModel } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./component/dashboard/dashboard.component";
 // import {HttpClientModule} from '@angular/common/http'
-// import { CommonModule } from "@angular/common";
+import { CommonModule } from "@angular/common";
 
+
+// wont work with this showing 
 // @new NgModel({
 //     declarations: [
 //         AppComponent,
@@ -20,4 +22,4 @@
 //     bootstrap: [AppComponent]
 // })
 
-// export class AppModule {}
+export class AppModule {}
